cf2eb49287841b7611c4abb7933e8c3a4b21231f
armyrunner <runner3@LAPTOP-4AJBJ0I9.localdomain>
2019-11-08 16:57:48 -0700

    revised

--- clientSide/app.js
+++ clientSide/app.js
@@ -1,28 +1,15 @@
 var browsebutton = document.querySelector("#fileFinder");
-browsebutton.onclick = function(){
+browsebutton.onclick = function (){
 
-	$.getJSON( browsebutton, function( data ) {
-		var items = [];
-		$.each( data, function( key, val ) {
-		  items.push( "<li id='" + key + "'>" + val + "</li>" );
-		});
-	   
-		$( "<ul/>", {
-		  "class": "my-new-list",
-		  html: items.join( "" )
-		}).appendTo( "body" );
-	  });
 
-	fetch("http://localhost:8080/files", {
-			method: "POST",
-			body: bodyStr,
-			headers:{
-				"Content-Type":"application/x-www-form-urlencoded"
-			}
+	fetch("http://localhost:8080/python", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded"
+		}
 
-		}).then(function(response){
-			console.log("Server Responed")
+	}).then(function(response) {
 
-		});
+
+	});
 }
-
