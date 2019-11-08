f3ca3fa555cdc0222d3cf13d1690b9f2145a2c72
ro_boat <rory.wagner@dixiesuccess.org>
2019-11-08 09:46:37 -0700

    I will start working on the server which will call the backend code.

--- serverSide/server.py
+++ serverSide/server.py
@@ -5,9 +5,21 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class MyRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        pass
+        
+        return
 
 
     def do_POST(self):
-        pass
 
+        return
+
+def run():
+    # this is the current computer address and port number:
+    listen = ("127.0.0.1", 8080)
+    server = HTTPServer(listen, MyRequestHandler)
+
+    print("Listening...")
+
+    server.serve_forever()
+
+run()
