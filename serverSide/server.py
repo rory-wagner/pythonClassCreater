59866637e21755bc7c253a504f3ff1509879e356
armyrunner <runner3@LAPTOP-4AJBJ0I9.localdomain>
2019-11-08 09:41:51 -0700

    template server

---
+++ serverSide/server.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+
+class MyRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        pass
+
+
+    def do_POST(self):
+        pass
+
